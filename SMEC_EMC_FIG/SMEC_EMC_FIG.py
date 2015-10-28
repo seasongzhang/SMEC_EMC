d3ac8b2a9276a1fb6d0b9c53fc40cb72ca28a1d2
seasongzhang <seasong.zhang@outlook.com>
2015-10-28 17:47:49 +0800

    Add main file.

---
+++ SMEC_EMC_FIG/SMEC_EMC_FIG.py
@@ -0,0 +1,18 @@
+﻿import Image
+
+im = Image.open(r"E:\Workspace\Python27\SMEC_EMC_FIG\TestData\fig1.png")
+width = im.size[0]
+height = im.size[1]
+
+count = 0
+
+for h in range(height):
+    for w in range(width):
+        pixel = im.getpixel((w,h))
+        if not pixel == (0,0,0,0):
+            print pixel
+
+
+pass
+#im.show()
+
