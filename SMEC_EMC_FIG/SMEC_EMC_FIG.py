6cfc4696ed74da56c3879d4927f115078fe48236
seasongzhang <seasong.zhang@outlook.com>
2015-10-29 22:26:58 +0800

    Pickle and tuple.sort used
    1 hour at home.

--- SMEC_EMC_FIG/SMEC_EMC_FIG.py
+++ SMEC_EMC_FIG/SMEC_EMC_FIG.py
@@ -1,45 +1,57 @@
 ﻿import Image
+import pickle
 
 RED = (255,0,0,255)
 BLUE = (0,255,255,255)
 
-im = Image.open(r"E:\Workspace\Python27\SMEC_EMC_FIG\TestData\fig1.png")
-im_data = im.getdata()
+im = Image.open(r"d:\Workspace\Python27\SMEC_EMC_FIG\TestData\fig1.png")
+im_data = list(im.getdata())
 width = im.size[0]
 height = im.size[1]
+
 data_list = []
+#red_list = []
+#blue_list = []
+#for n in range(len(im_data)):
+#    if im_data[n] == RED:
+#        #print im_data[n]
+#        red_list.append((n % width, n / width))
+#    elif im_data[n] == BLUE:
+#        blue_list.append((n % width, n / width))
 
-red_list = []
-blue_list = []
-for n in range(len(im_data)):
-    if im_data[n] == RED:
-        #print im_data[n]
-        red_list.append((n / width, n % width))
-    elif im_data[n] == BLUE:
-        blue_list.append((n / width, n % width))
+#with open("red_list.pkl",'wb') as f:
+#    pickle.dump(red_list,f)
+#with open("blue_list.pkl","wb") as f:
+#    pickle.dump(blue_list,f)
 
-for (x,y) in blue_list:
-    im.putpixel((x/2,y/2),RED)
+with open("red_list.pkl",'rb') as f:
+    red_list = pickle.load(f)
+with open("blue_list.pkl","rb") as f:
+    blue_list = pickle.load(f)
+
+blue_list.sort()
+red_list.sort()
+
+def pickMaxPoints(xy_list):
+    xy_max_list = []
+    x_list = []
+    for (x,y) in xy_list:
+        cc = (x,y)
+        if x not in x_list:
+            x_list.append(x)
+            xy_max_list.append((x,y))
+    return xy_max_list
+
+blue_list2 = pickMaxPoints(blue_list)        
+
+for (x,y) in blue_list2:
+    im.putpixel((x,y),RED)
+
+for (x,y) in red_list:
+    im.putpixel((x,y),(0,255,0,255))
+
+with open("blue_list.txt","w") as f:
+    for xy in blue_list2:
+        f.write(str(xy[0])+"\t"+str(xy[1])+"\n")
 
 im.show()
-        #data_set = set(data_list)
-
-#im.show()
-count = 0
-print im.format,im.size,im.mode
-
-pixel_set = []
-
-#for h in range(height):
-#    for w in range(width):
-#        pixel = im.getpixel((w,h))
-#        if pixel not in pixel_set:
-#            pixel_set.append(pixel)
-#            print pixel
-
-#print pixel_set
-
-
-pass
-#im.show()
-
