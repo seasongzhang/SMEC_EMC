8eab394313ae3cdf5843902f265496a4ee432f11
seasongzhang <seasong.zhang@outlook.com>
2015-10-30 16:31:21 +0800

    Python3.5
    1 hour in office

--- SMEC_EMC_FIG/SMEC_EMC_FIG.py
+++ SMEC_EMC_FIG/SMEC_EMC_FIG.py
@@ -1,57 +1,72 @@
 ﻿from PIL import Image
 import pickle
 
-RED = (255,0,0,255)
-BLUE = (0,255,255,255)
 
-im = Image.open(r"E:\Workspace\Python27\SMEC_EMC_FIG\TestData\fig1.png")
-im_data = list(im.getdata())
-width = im.size[0]
-height = im.size[1]
+class EmcSpectrumPng:
 
-data_list = []
-#red_list = []
-#blue_list = []
-#for n in range(len(im_data)):
-#    if im_data[n] == RED:
-#        #print im_data[n]
-#        red_list.append((n % width, n / width))
-#    elif im_data[n] == BLUE:
-#        blue_list.append((n % width, n / width))
+    COLOR_LIMIT = (255,0,0,255)
+    COLOR_SPECTRUM = (0,255,255,255)
+    CROP_BOX = (321,734,2300,2138)
+    HEIGHT_DB = 140
 
-#with open("red_list.pkl",'wb') as f:
-#    pickle.dump(red_list,f)
-#with open("blue_list.pkl","wb") as f:
-#    pickle.dump(blue_list,f)
+    def __init__(self, file_path):
+        self.im = Image.open(file_path)
+    #TODO:
+        
 
-with open("red_list.pkl",'rb') as f:
-    red_list = pickle.load(f)
-with open("blue_list.pkl","rb") as f:
-    blue_list = pickle.load(f)
+    "Read image into python and crop useful window and name it 'region'."
 
-blue_list.sort()
-red_list.sort()
+    region = im.crop(CROP_BOX)
+    region_data = list(region.getdata())
+    width = region.size[0]
+    height = region.size[1]
+    pixels_per_dB = height / HEIGHT_DB
 
-def pickMaxPoints(xy_list):
-    xy_max_list = []
-    x_list = []
-    for (x,y) in xy_list:
-        cc = (x,y)
-        if x not in x_list:
-            x_list.append(x)
-            xy_max_list.append((x,y))
-    return xy_max_list
+    #region.show()
 
-blue_list2 = pickMaxPoints(blue_list)        
+    data_list = []
+    red_list = []
+    blue_list = []
+    #for n in range(len(region_data)):
+    #    if region_data[n] == RED:
+    #        red_list.append((n % width, n // width))
+    #    elif region_data[n] == BLUE:
+    #        blue_list.append((n % width, n // width))
 
-for (x,y) in blue_list2:
-    im.putpixel((x,y),RED)
+    #with open("red_list.pkl",'wb') as f:
+    #    pickle.dump(red_list,f)
+    #with open("blue_list.pkl","wb") as f:
+    #    pickle.dump(blue_list,f)
 
-for (x,y) in red_list:
-    im.putpixel((x,y),(0,255,0,255))
+    with open("red_list.pkl",'rb') as f:
+        red_list = pickle.load(f)
+    with open("blue_list.pkl","rb") as f:
+        blue_list = pickle.load(f)
 
-with open("blue_list.txt","w") as f:
-    for xy in blue_list2:
-        f.write(str(xy[0])+"\t"+str(xy[1])+"\n")
+    blue_list.sort()
+    red_list.sort()
 
-im.show()
+    def pickMaxPoints(xy_list):
+        xy_max_list = []
+        x_list = []
+        for (x,y) in xy_list:
+            if x not in x_list:
+                x_list.append(x)
+                xy_max_list.append((x,y))
+        return xy_max_list
+
+    blue_list = pickMaxPoints(blue_list)        
+
+    for (x,y) in blue_list:
+        region.putpixel((x,y),RED)
+
+    for (x,y) in red_list:
+        region.putpixel((x,y),(0,255,0,255))
+
+    raw_spectrum = [(10**(xy[0]/1300)*30,(46 - (xy[1]-632)/pixels_per_dB)) for xy in blue_list]
+
+    with open("blue_list.txt","w") as f:
+        for xy in blue_data_dB:
+            f.write(str(xy[0])+"\t"+str(xy[1])+"\n")
+
+    region.show()
