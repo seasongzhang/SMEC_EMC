cc6ca4248277bf0f66b3e667d1ec2723298f914d
Seasong <seasong.zhang@outlook.com>
2015-11-09 17:13:35 +0800

    almost done

--- SMEC_EMC_FIG/SMEC_EMC_FIG.py
+++ SMEC_EMC_FIG/SMEC_EMC_FIG.py
@@ -5,64 +5,70 @@ from PIL import Image
 import pickle
 import os
 import re
-import numpy
-import matplotlib.pyplot as pl
-# pl.rcParams['font.sans-serif'] = ['SimHei'] # For plotting Chinese characters
 
-class EmcSpecPng:
 
+# import numpy
+# import matplotlib.pyplot as pl
+
+class SpecReader:
     COLOR_LIMIT = (255, 0, 0, 255)
     COLOR_SPECTRUM = (0, 255, 255, 255)
     GREEN = (0, 255, 0, 255)
-    CROP_BOX = (321, 734, 2300, 2138) #
+    CROP_BOX = (321, 734, 2300, 2138)
     HEIGHT_DB = 120.0
 
-    def __init__(self, file_path):
-        self.file_path = file_path
-        self.im = Image.open(self.file_path)
-    #TODO:
-    
-    def size_check(self):
-        assert self.im.size[0] == 3197
-        assert self.im.size[1] == 2455
-        
-    def read_data(self):
-        "Read image(png) into python and crop useful window and name it 'region'."
+    def __init__(self):
+        pass
+        """
+            Read png object from file_path.
+        """
 
-        region = self.im.crop(EmcSpecPng.CROP_BOX)
-        region_data = list(region.getdata()) # get RGB value for each pixel
-        width = region.size[0] 
+    def read_spec_from_png(self, file_path, save2txt=False):
+        """
+            Read image(png) into python and crop useful window and name it 'region'.
+            :param save2txt:
+            :return:
+        """
+        im = Image.open(file_path)
+
+        assert im.size[0] == 3197
+        assert im.size[1] == 2455
+
+        region = im.crop(SpecReader.CROP_BOX)
+        region_data = list(region.getdata())  # get RGB value for each pixel
+        width = region.size[0]
         height = region.size[1]
-        pixels_per_dB = height / EmcSpecPng.HEIGHT_DB
+        pixels_per_dB = height / SpecReader.HEIGHT_DB
 
         # region.show()
-        
+
         """
-        According to RGB value of pixels, select pixels of limit named limit_list
-        and spectrum named peak_value_list. Since region.getdata() get pixels line by line,
-        so the limit_list and peak_list are not lined in frequency sequence. Thus sort() is 
-        used to sort. Besides, we only care about the max spectrum point for each frequency 
-        point, thus pick_max_point() is applied.
+            According to RGB value of pixels, select pixels of limit named limit_list
+            and spectrum named peak_value_list. Since region.getdata() get pixels line by line,
+            so the limit_list and peak_list are not lined in frequency sequence. Thus sort() is
+            used to sort. Besides, we only care about the max spectrum point for each frequency
+            point, thus pick_max_point() is applied.
         """
         data_list = []
         limit_list = []
         peak_list = []
 
-        #for n in range(len(region_data)):
-        #    if region_data[n] == EmcSpecPng.COLOR_LIMIT:
-        #        limit_list.append((n % width, n // width))
-        #    elif region_data[n] == EmcSpecPng.COLOR_SPECTRUM:
-        #        peak_list.append((n % width, n // width))
+        # Extract spectrum pixel and limit pixel from png seperately according RGB value
+        for n in range(len(region_data)):
+            if region_data[n] == SpecReader.COLOR_LIMIT:
+                limit_list.append((n % width, n // width))
+            elif region_data[n] == SpecReader.COLOR_SPECTRUM:
+                peak_list.append((n % width, n // width))
 
-        #with open("limit_list.pkl",'wb') as f:
+        # with open("limit_list.pkl",'wb') as f:
         #    pickle.dump(limit_list,f)
-        #with open("peak_list.pkl","wb") as f:
+        # with open("peak_list.pkl","wb") as f:
         #    pickle.dump(peak_list,f)
 
-        with open("limit_list.pkl",'rb') as f:
-            limit_list = pickle.load(f)
-        with open("peak_list.pkl","rb") as f:
-            peak_list = pickle.load(f)
+        # with open("limit_list.pkl", 'rb') as f:
+        #     limit_list = pickle.load(f)
+        # with open("peak_list.pkl", "rb") as f:
+        #     peak_list = pickle.load(f)
 
         peak_list.sort()
         limit_list.sort()
@@ -70,59 +76,48 @@ class EmcSpecPng:
         def pick_max_point(xy_list):
             xy_max_list = []
             x_list = []
-            for (x,y) in xy_list:
+            for (x, y) in xy_list:
                 if x not in x_list:
                     x_list.append(x)
-                    xy_max_list.append((x,y))
+                    xy_max_list.append((x, y))
             return xy_max_list
 
-        peak_list = pick_max_point(peak_list)        
+        peak_list = pick_max_point(peak_list)  # for each frequency, only keep the max value.
 
-        for (x,y) in peak_list:
-            region.putpixel((x,y), EmcSpecPng.GREEN)
+        # "Transfer spectrum from pixel data to regular data, x-axis is MHz, y-axis is dB"
+        decimal_spectrum = [(10 ** (xy[0] / 1300.0) * 30, (46 - (xy[1] - 632) / pixels_per_dB)) for xy in peak_list]
 
-        for (x,y) in limit_list:
-            region.putpixel((x,y), EmcSpecPng.GREEN)
+        # Write decimal_spectrum to a txt file
+        if save2txt == True:
+            with open(os.path.splitext(file_path)[0] + ".txt", "w") as f:
+                for sp in decimal_spectrum:
+                    print('%(freq).2f %(dB).2f' % {'freq': sp[0], 'dB': sp[1]}, file=f, sep='')
 
-        "Transfer spectrum from pixel data to regular data, x-axis is MHz, y-axis is dB"
-        self.raw_spectrum = [(10**(xy[0]/1300.0)*30,(46 - (xy[1]-632)/pixels_per_dB)) for xy in peak_list]
-        
-    def dump2txt(self):
-        with open(os.path.splitext(self.file_path)[0] + ".txt", "w") as f:
-            for sp in self.raw_spectrum:
-                print('%(freq).2f %(dB).2f' % {'freq':sp[0],'dB':sp[1]},file=f,sep='')
+        return decimal_spectrum
 
-    def dump2fig(self):
-        pl.figure()
-        fig_name = os.path.splitext(self.file_path)[0] + "_plot.png"
-        #data_len = len(self.raw_spectrum[0])
-        plot_data = numpy.transpose(self.raw_spectrum)
-        plot_data = list(plot_data)
-        pl.semilogx(plot_data[0],plot_data[1],basex = 10)
-        pl.title("AAA")
-        pl.xlim(plot_data[0][0],plot_data[0][-1])
-        pl.ylim(-20,100)
-        pl.axhline(y=46, xmin=0, xmax=0.8846, color = 'r')
-        pl.axhline(y=53, xmin=0.8846, xmax=1, color = 'r')
-        pl.axvline(x=230, ymin=0.55, ymax=0.6083, color = 'r')
-        pl.xlabel("Frequency")
-        pl.ylabel("Level")
-        pl.grid(b=True,which='both')
-        pl.savefig(fig_name)
+    def gather_specs(self, dir_path, re_str=u'.png'):
+        """
+            Gather all the spectrum data in dir_path.
+        :return:
+        """
+        decimal_specs = []
+        files = [f for f in os.listdir(dir_path) if re.match(re_str, f)]
+        try:
+            files.sort(key=lambda n:int(n.split("_")[1].split('.')[0]))
+        except IndexError:
+            print("Png files not named corrected.")
+        for file in files:
+            file_path = os.path.join(dir_path, file)
+            if re.search(re_str, file):
+                print("Processing " + file)
+                try:
+                    decimal_specs.append(self.read_spec_from_png(file_path, save2txt=True))
+                except AssertionError:
+                    print("Png size not corrected! File path" + file_path)
+        return decimal_specs
 
 if __name__ == "__main__":
-    dir_path = u"E:\\SeaGit\\SMEC_EMC_FIG\\P1_Test"
-    files = os.listdir(dir_path)
-    for file in files:
-        file_path = os.path.join(dir_path,file)
-        if re.search(u"P1_1.png",file):
-            print("Processing "+file)
-            png = EmcSpecPng(file_path)
-            try:
-                png.size_check()
-            except AssertionError:
-                print("Png size not corrected!")
-            else:
-                png.read_data()
-                #png.dump2txt()
-                png.dump2fig()
+    pngset = SpecReader()
+    x = pngset.gather_specs(ur'E:\SeaGit\SMEC_EMC_FIG\testdata\MPS1_P1_CAN\data', re_str='P1_.+\.png')
+    with open(ur'E:\SeaGit\SMEC_EMC_FIG\testdata\MPS1_P1_CAN\data' + ur'\decimal_specs.pkl', 'wb') as f:
+        pickle.dump(x, f)
--- SMEC_EMC_FIG/datachk.py
+++ SMEC_EMC_FIG/datachk.py
@@ -2,9 +2,13 @@
 import os
 import codecs
 import re
+import numpy
+import matplotlib.pyplot as pl
+import pickle
 
+import SMEC_EMC_FIG
 
-class DataChecker(object):
+class ReportMaker(object):
     """
         This class if used for 
     """
@@ -16,13 +20,12 @@ class DataChecker(object):
         self.steps = []
         self.project = []
 
-    def check(self, file_name, *args):
+    def read_info(self, file_name, *args):
         """
             Check file format.
             :param args: a list, in which sequence the raw data txt is listed.
             :param file_name: the raw data txt file name.
         """
-
         if os.path.exists(os.path.join(self.dir_path, file_name)):
             items = []
             with codecs.open(os.path.join(self.dir_path, file_name), encoding='utf-8') as f:
@@ -45,14 +48,44 @@ class DataChecker(object):
                     f.write("\r\n")
             return []
 
-    def allcheck(self):
-        self.parts = self.check("parts.txt", "part_number", "part_name", "part_description", "part_photos")
-        self.actions = self.check("actions.txt", "action_number", "action_description", "action_photos")
-        self.steps = self.check("steps.txt", "step_sequence", "step_ref", "step_description", "step_reason",
+    def gather_infos(self):
+        self.parts = self.read_info("parts.txt", "part_number", "part_name", "part_description", "part_photos")
+        self.actions = self.read_info("actions.txt", "action_number", "action_description", "action_photos")
+        self.steps = self.read_info("steps.txt", "step_sequence", "step_ref", "step_description", "step_reason",
                                 "step_outputfile", "step_result", "step_summary", "step_spectrum")
-        self.project = self.check("project.txt", "project_date", "project_description", "project_summary")
+        self.project = self.read_info("project.txt", "project_date", "project_description", "project_summary")
 
-    def report(self):
+    def compare_plot(self):
+
+        with open(ur'E:\SeaGit\SMEC_EMC_FIG\testdata\MPS1_P1_CAN\data' + ur'\decimal_specs.pkl', 'rb') as f:
+            specs = pickle.load(f)
+
+        if not len(specs) == len(self.steps):
+            print("Steps number is different from spectrum data number!!!")
+
+        for (n, step) in enumerate(self.steps):
+            compare_list = step[1].split(',')
+            pl.figure()
+            plot_name = self.dir_path + ur'\photos\STEP_' + str(n+1) + ur'.png'
+            plot_data = list(numpy.transpose(specs[n]))
+            pl.semilogx(plot_data[0], plot_data[1], label=u'STEP'+str(n+1), basex=10)
+            if not compare_list == [u'']:
+                for c in compare_list:
+                    plot_data = list(numpy.transpose(specs[int(c)-1]))
+                    pl.semilogx(plot_data[0], plot_data[1], label=u'STEP'+str(int(c)), basex=10)
+            pl.title("STEP"+str(n+1))
+            pl.xlim(plot_data[0][0], plot_data[0][-1])
+            pl.ylim(-20,100)
+            pl.axhline(y=46, xmin=0, xmax=0.8846, color='r')
+            pl.axhline(y=53, xmin=0.8846, xmax=1, color='r')
+            pl.axvline(x=230, ymin=0.55, ymax=0.6083, color='r')
+            pl.xlabel("Frequency")
+            pl.ylabel("Level")
+            pl.grid(b=True, which='both')
+            pl.legend()
+            pl.savefig(plot_name)
+
+    def make_markdown(self):
 
         # 将措施表中的部件名加``，作为修饰。
         for part in [p[1] for p in self.parts if not p[1]=='']:
@@ -68,10 +101,10 @@ class DataChecker(object):
         actions_dict = dict((action[0], action[1]) for action in self.actions)
 
         with codecs.open(os.path.join(self.dir_path, "report.md"), "w",encoding='utf-8') as f:
-            f.write(u"#1.测试日期：\r\n" + self.project[0][0] + "\r\n")
-            f.write(u"#2.测试描述：\r\n" + self.project[0][1] + "\r\n")
-            f.write(u"#3.测试总结：\r\n" + self.project[0][2] + "\r\n")
-            f.write(u"#4.测试步骤：\r\n")
+            f.write(u"##1.测试日期：\r\n" + self.project[0][0] + "\r\n")
+            f.write(u"##2.测试描述：\r\n" + self.project[0][1] + "\r\n")
+            f.write(u"##3.测试总结：\r\n" + self.project[0][2] + "\r\n")
+            f.write(u"##4.测试步骤：\r\n")
             for step in self.steps:
                 f.write("####STEP " + step[0] + "\r\n")
                 f.write(u"- **测试条件描述：**")
@@ -81,11 +114,11 @@ class DataChecker(object):
                 f.write(u'- **步骤依据：**' + step[3] + '\r\n'*2)
                 f.write(u'- **测试结果：**' + step[5] + '\r\n'*2)
                 f.write(u'- **测试小结：**' + step[6] + '\r\n'*2)
-                x = self.dir_path + u'\photos\step_' + step[0] + u'.png'
+                x = self.dir_path + u'\photos\STEP_' + step[0] + u'.png'
                 print(os.path.exists(x))
                 if os.path.exists(x):
                     print("Hello!")
-                    f.write(u'![STEP' + step[0] + ur'](..\testdata\MPS1\_P1\_CAN\photos\step_' + step[0] +u'.png)\r\n')
+                    f.write(u'![STEP' + step[0] + ur'](.\photos\STEP_' + step[0] +u'.png)\r\n')
 
             f.write(u'___\r\n')
             f.write(u"#部件表：\r\n")
@@ -93,7 +126,10 @@ class DataChecker(object):
                 f.write(u'- <span id='+part[1]+u'>`'+part[1]+u'`</span>')
                 f.write(u'：' + part[2] + u'\r\n'*2)
 
+
 if __name__ == "__main__":
-    chker = DataChecker(ur"E:\SeaGit\SMEC_EMC_FIG\testdata\MPS1_P1_CAN")
-    chker.allcheck()
-    chker.report()
+    chker = ReportMaker(ur"E:\SeaGit\SMEC_EMC_FIG\testdata\MPS1_P1_CAN")
+    chker.gather_infos()
+    chker.compare_plot()
+    chker.make_markdown()
+
