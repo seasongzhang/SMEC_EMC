8371cc595636166a6c0fd2b7d0abca1be4225f54
Seasong@EIS-846 <seasong.zhang@outlook.com>
2016-03-09 23:08:49 +0800

    Add txt check code @home

--- SmecEmcReport/check.py
+++ SmecEmcReport/check.py
@@ -1,39 +1,50 @@
+import os
+import codecs
+
+
 class Chker:
     def __init__(self, prt_path):
-        self.prt_path = prt_path
-        self.txt_set = {'project.txt', 'parts.txt', 'actions.txt', 'steps.txt'}
+        self.txt_path = os.path.join(prt_path, "txt")
+        self.txt_list = ['project.txt', 'parts.txt', 'actions.txt', 'steps.txt']
+        self.txt_set = set(self.txt_list)
+        self.txt_format = {self.txt_list[0]: ['project_date', 'project_description', 'project_summary'],
+                           self.txt_list[1]: ['part_number', 'part_name', 'part_description'],
+                           self.txt_list[2]: ['action_number', 'action_description'],
+                           self.txt_list[3]: ['step_sequence', 'step_ref', 'step_description', 'step_reason',
+                                              'step_outputfile', 'step_result', 'step_summary']}
 
-    def txts_exist(self):
+    def txt_exist(self):
         try:
-            # txt_list = os.listdir( os.path.join( self.prt_path, "txt" ) )
-            txt_list = ['project.txt', 'parts.txt', 'actions.txt', 'teps.txt']
+            txt_list = os.listdir(self.txt_path)
+            # txt_list = ['project.txt', 'parts.txt', 'actions.txt', 'teps.txt']
         except WindowsError:
             print("Error: directory 'txt' missing.")
             return False
 
-        def print_txt_state(flist, state_str):
-            for x in flist:
+        def print_txt_state(file_set, state_str):
+            for x in file_set:
                 print("file %s is " % x + state_str)
 
-        if not self.txt_set == set( txt_list ):
-            print_txt_state( self.txt_set - set( txt_list ), "missing." )
-            print_txt_state( set( txt_list ) - self.txt_set, "useless." )
+        if not self.txt_set == set(txt_list):
+            print_txt_state(self.txt_set - set(txt_list), "missing.")
+            print_txt_state(set(txt_list) - self.txt_set, "useless.")
             return False
+        return True
 
+    def has_right_format(self):
+        for txt in self.txt_set:
+            with codecs.open(os.path.join(self.txt_path, txt), encoding='utf-8') as f:
+                lines = [line for line in f.readlines() if ":" in line]
+                for (n, line) in enumerate(lines):
+                    txt_format = self.txt_format[txt]
+                    if not line.startswith(txt_format[n % len(txt_format)]):
+                        print("Wrong format! Line: " + line)
+                        return False
+        return True
 
-c = Chker( 'a' )
-c.txts_exist( )
-# def has_right_format(self):
-#     if os.path.exists( os.path.join( self.dir_path, file_name ) ):
-#         items = []
-#         with codecs.open( os.path.join( self.dir_path, file_name ), encoding='utf-8' ) as f:
-#             lines = [line for line in f.readlines( ) if (not line == "\r\n") and (not line == "\n")]
-#             for (n, line) in enumerate( lines ):
-#                 try:
-#                     assert line.startswith( args[n % len( args )] )
-#                 except AssertionError:
-#                     print(n, len( args ), line)
-#                     print("Wrong format of data in " + file_name + ", number " + str( (n % len( args )) + 1 ))
-#                 else:
-#                     items.append( line.split( ":" )[1].strip( ) )
-#         items = [items[n:n + len( args )] for n in xrange( 0, len( items ), len( args ) )]
+    def txt_checked(self):
+        return self.txt_exist() and self.has_right_format()
+
+c = Chker(r"E:\Downloads\CS_JT150795B000G99")
+if c.txt_checked():
+    print('Pass.')
